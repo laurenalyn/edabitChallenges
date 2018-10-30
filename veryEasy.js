9b7d42879a1f20594790ca00fe11e73c5fd9b83e
laurenalyn <laurenalynwilkerson@gmail.com>
2018-10-30 12:04:06 -0400

    functrion that takes 2 strings and returns t/f depending on if their lengths are equal or not

---
+++ veryEasy.js
@@ -0,0 +1,9 @@
+// Create a function that takes two strings as arguments and return either true or false
+//depending on whether the number of their charcters is equal or not.
+function comp(str1, str2) {
+  if (str1.length === str2.length) {
+    return true;
+    } else {
+            return false;
+    }
+}
