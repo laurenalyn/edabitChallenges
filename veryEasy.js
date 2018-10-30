d8c9ac218d78a0b259c15bf84a063a8d639661a9
laurenalyn <laurenalynwilkerson@gmail.com>
2018-10-30 13:40:06 -0400

    function that takes a number and increments the number by 1

--- veryEasy.js
+++ veryEasy.js
@@ -1,5 +1,5 @@
 // Create a function that takes two strings as arguments and return either true or false
-//depending on whether the number of their charcters is equal or not.
+// depending on whether the number of their charcters is equal or not.
 function comp(str1, str2) {
   if (str1.length === str2.length) {
     return true;
@@ -13,3 +13,8 @@ function addition(a, b) {
     return a + b
 }
 
+// Create a function that takes a number as an argument, increments the number
+// by +1 and returns the result.
+function addition(num) {
+	return num + 1;
+}
