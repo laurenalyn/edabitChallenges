11dc9b871bbdef7ef81f576c0c81b078a5389fbe
laurenalyn <laurenalynwilkerson@gmail.com>
2018-10-31 14:25:16 -0400

    function that takes a number and return t/f if its <= 0

--- veryEasy.js
+++ veryEasy.js
@@ -49,3 +49,14 @@ function month_name(num) {
             break;
     }
 }
+
+// Create a function that takes a number as its only argument and
+// returns true if it's less than or equal to zero, otherwise return false.
+function lessThanOrEqualToZero(num) {
+	if (num <= 0 ){
+		return true
+	} else {
+		return false
+	}
+}
+
