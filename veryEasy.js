94ee69be9f0d9fb85dac991b0e8064a3215465f2
laurenalyn <laurenalynwilkerson@gmail.com>
2018-10-31 14:59:57 -0400

    functin that takes an arr and str and return the index of the str

--- veryEasy.js
+++ veryEasy.js
@@ -76,4 +76,10 @@ function isEvenOrOdd(num) {
     }
 }
 
+// Create a function that takes an array and a string as arguments
+// and return the index of the string.
+function find_index(arr, str) {
+    return arr.indexOf(str)
+}
+
 // 
