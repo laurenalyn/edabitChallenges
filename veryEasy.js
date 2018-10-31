5e58beaeec3fa2a1801e6c7122366542ce6a0565
laurenalyn <laurenalynwilkerson@gmail.com>
2018-10-31 15:18:39 -0400

    function that takes a base num and an exponent and return the calculation

--- veryEasy.js
+++ veryEasy.js
@@ -82,4 +82,8 @@ function find_index(arr, str) {
     return arr.indexOf(str)
 }
 
-// 
+// Create a function that takes a base number and an exponent
+// number and returns the calculation.
+function calculateExponent(num, exp) {
+	return Math.pow(num, exp);
+}
