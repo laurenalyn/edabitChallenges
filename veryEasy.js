168ac6dcd4ba9017ddaafc9c92fefffe77c16de0
laurenalyn <laurenalynwilkerson@gmail.com>
2018-11-02 14:43:12 -0400

    creates a function that takes an array of numbers and returns the smalled number in the set

--- veryEasy.js
+++ veryEasy.js
@@ -127,3 +127,8 @@ function Go(num) {
 function Go(num) {
 	return "-".repeat(num);
 }
+
+// Create a function that takes an array of numbers and returns the smallest number in the set.
+function findSmallestNum(arr) {
+	return Math.min(...arr);
+}
