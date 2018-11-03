96162486df28cf9b198714d419f5c6c191401851
laurenalyn <laurenalynwilkerson@gmail.com>
2018-11-03 13:38:34 -0400

    creates a function that takes a string as its argument and returns the string in reversed order.

--- veryEasy.js
+++ veryEasy.js
@@ -163,3 +163,8 @@ function reverse(bool) {
 function nameShuffle(str) {
 	return str.split(" ").reverse().join(" ");
 }
+
+//Create a function that takes a string as its argument and returns the string in reversed order.
+function reverse(str) {
+	return str.split("").reverse().join("");
+}
